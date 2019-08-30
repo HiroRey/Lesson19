83fbb58214383cd38999b8030a815bee7218e602
Ivan <barrowkiss@gmail.com>
2019-08-31 00:46:05 +0300

    Create project

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+$searchWords = ['php','html','интернет','Web'];
+
+
+$searchStrings = [
+    'Интернет - большая сеть компьютеров, которые могут взаимодействовать друг с другом.',
+    'PHP - это распространенный язык программирования с открытым исходным кодом.',
+    'PHP сконструирован специально для ведения Web-разработок и его код может внедряться непосредственно в HTML'
+];
+
+
+ function wordsReview($searchWords,$searchStrings)
+ {
+
+     foreach ($searchStrings as $key => $searchString) {
+         $res = '';
+
+
+         foreach ($searchWords as $searchWord) {
+
+             $allWords = '/'.$searchWord.'/ui';
+
+
+             if (preg_match($allWords, $searchString, $arr)) {
+                 $res = $res . ',' .  $arr[0];
+             }
+         }
+
+
+         echo 'В предложении ' . ($key+=1) . ' есть слова: ' . $res . '<br>';
+
+
+     }
+ }
+
+
+
+
+wordsReview($searchWords,$searchStrings);
